ab9284f94f5ca1dfeb10a2e28e57eb2cf85afde2
khuong291 <dkhuong291@gmail.com>
2018-06-27 14:36:14 +0700

    Update Notification.jsx

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,15 +1,91 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 
-class Notification extends React.Component {}
+class Notification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isRunning: true
+      // flag = {
+      //   canCloseOnClick: true,
+      //   canDrag: false
+      // },
+      // drag = {
+      //   start: 0,
+      //   x: 0,
+      //   deltaX: 0,
+      //   removalDistance: 0
+      // }
+    };
+  }
 
-Notification.PropTypes = {
-  closeButton: boolean.isRequired,
-  autoClose: boolean.is,
-  draggable: boolean,
-  onOpen: func.isRequired,
-  onClose: func.isRequired,
-  onClick: func.isRequired
+  componentDidMount() {
+    this.props.onOpen(this.props.children.props);
+    if (this.props.draggable) {
+      this.bindDragEvents();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.draggable !== this.props.draggable) {
+      this.props.draggable ? this.bindDragEvents() : this.unbindDragEvents();
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.onClose(this.props.children.props);
+    if (this.props.draggable) {
+      this.unbindDragEvents();
+    }
+  }
+
+  pauseToast = () => {
+    this.setState({ isRunning: false });
+  };
+
+  playToast = () => {
+    this.setState({ isRunning: true });
+  };
+
+  bindDragEvents() {
+    document.addEventListener("mousemove", this.onDragMove);
+    document.addEventListener("mouseup", this.onDragEnd);
+
+    document.addEventListener("touchmove", this.onDragMove);
+    document.addEventListener("touchend", this.onDragEnd);
+  }
+
+  unbindDragEvents() {
+    document.removeEventListener("mousemove", this.onDragMove);
+    document.removeEventListener("mouseup", this.onDragEnd);
+
+    document.removeEventListener("touchmove", this.onDragMove);
+    document.removeEventListener("touchend", this.onDragEnd);
+  }
+
+  render() {
+    const { children } = this.props;
+    return <div>{children}</div>;
+  }
+}
+
+Notification.propTypes = {
+  children: PropTypes.node,
+  hasCloseButton: PropTypes.boolean.isRequired,
+  autoClose: PropTypes.boolean.isRequired,
+  draggable: PropTypes.boolean.isRequired,
+  onOpen: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired
+};
+
+Notification.defaultProps = {
+  hasCloseButton: false,
+  autoClose: true,
+  draggable: true,
+  onOpen: () => {},
+  onClose: () => {},
+  onClick: () => {}
 };
 
 export default Notification;
